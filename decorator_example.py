d66b41534e054b77428defeeec55e3d14a2eac7f
ben <cravenbenj@myvuw.ac.nz>
2021-12-02 12:36:18 +1300

    Explain decorators, which are used in ryu

---
+++ decorator_example.py
@@ -0,0 +1,50 @@
+"""
+Ryu uses decorators a lot.
+This is a simple example program for me to learn how to use
+them. 
+Decorators change the behaviour of a function without
+modifying the function itself.
+"""
+
+#functions are objects
+def my_function():
+    print('I am a function')
+
+a_function = my_function
+
+a_function()
+#output: I am a function
+
+#you can nest functions
+def outer_function():
+    def inner_function():
+        print("Inside the inner function")
+    inner_function()
+
+outer_function()
+#Output: I came from the inner function
+#calling inner_function() returns an error
+#it is only available locally inside outer_function
+#decorators take the following format
+"""
+def function_decorator(func):
+    def wrapped_func():
+        #do something before function
+        func()
+        #do something after function
+    return wrapped_func
+"""
+
+#simple example: add borders to a print statement
+def add_border(func):
+    def wrapped_func():
+        print('=' * 30)
+        func()
+        print('=' * 30)
+    return wrapped_func
+
+@add_border
+def hello_world():
+    print('Hello world!')
+
+hello_world()
